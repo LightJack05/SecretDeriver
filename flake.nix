216b192d63a99a66b5be77f27f4a991e9064dcc7
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-04-04 23:29:11 +0200

    Added helmify and kustomize to flake

--- flake.nix
+++ flake.nix
@@ -8,6 +8,21 @@
 
   outputs = { self, nixpkgs, go-license-collector, ... }:
     let
+      helmify = pkgs.buildGoModule rec {
+        pname = "helmify";
+        version = "0.4.19";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "arttor";
+          repo = "helmify";
+          rev = "v${version}";
+          hash = "sha256-casZJRHpTbakI1FdSYPYcda2G8xPP+feTQil6R+FdbE=";
+        };
+        vendorHash = "sha256-ShX11hDA8oPkvT37vYArL8VzthfWdxbq9mvpYuL/0gE=";
+
+        subPackages = [ "cmd/helmify" ];
+      };
+
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
@@ -17,6 +32,8 @@
           pkgs.kind
           pkgs.kubectl
           pkgs.operator-sdk
+          pkgs.kustomize
+          helmify
           go-license-collector.packages.${system}.go-license-collector
         ];
 
