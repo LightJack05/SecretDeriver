5027829fe3a25cad34a48efcf396a9df3fb3b920
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-04-20 21:43:09 +0200

    Added first test and set up table

--- internal/controller/derivedsecret_controller_test.go
+++ internal/controller/derivedsecret_controller_test.go
@@ -1,68 +1,99 @@
 package controller
 
 import (
-	"context"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	secretderiverv1alpha1 "github.com/LightJack05/SecretDeriver/api/v1alpha1"
 )
 
 var _ = Describe("DerivedSecret Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
-
-		ctx := context.Background()
-
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
-		}
-		derivedsecret := &secretderiverv1alpha1.DerivedSecret{}
-
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind DerivedSecret")
-			err := k8sClient.Get(ctx, typeNamespacedName, derivedsecret)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &secretderiverv1alpha1.DerivedSecret{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
+	DescribeTable("Given a DerivedSecret resource instance",
+		func(parentSecret *corev1.Secret, derivedSecretPhase string, derivedSecretConditionReason string, expectedDerivedSecret *corev1.Secret) {
+			Expect(k8sClient.Create(ctx, parentSecret)).To(Succeed())
+			// Create the DerivedSecret resource
+			derivedSecret := &secretderiverv1alpha1.DerivedSecret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      expectedDerivedSecret.Name,
+					Namespace: expectedDerivedSecret.Namespace,
+				},
+				Spec: secretderiverv1alpha1.DerivedSecretSpec{
+					ParentSecretRef: v1.SecretReference{
+						Name:      parentSecret.Name,
+						Namespace: parentSecret.Namespace,
 					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+					ParentSecretKey:    "key1",
+					GeneratedSecretKey: "key1",
+				},
 			}
-		})
+			Expect(k8sClient.Create(ctx, derivedSecret)).To(Succeed())
 
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &secretderiverv1alpha1.DerivedSecret{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
+			// Eventually, the controller should update the DerivedSecret status
+			crKey := types.NamespacedName{Name: derivedSecret.Name, Namespace: derivedSecret.Namespace}
+			Eventually(func(g Gomega) {
+				updatedDerivedSecret := &secretderiverv1alpha1.DerivedSecret{}
+				err := k8sClient.Get(ctx, crKey, updatedDerivedSecret)
+				g.Expect(err).ToNot(HaveOccurred())
+				g.Expect(updatedDerivedSecret.Status.Phase).To(Equal(derivedSecretPhase))
+				g.Expect(updatedDerivedSecret.Status.Conditions).ToNot(BeEmpty())
+				g.Expect(updatedDerivedSecret.Status.Conditions[0].Reason).To(Equal(derivedSecretConditionReason))
+			}).WithTimeout(time.Second * 10).WithPolling(time.Millisecond * 200).Should(Succeed())
 
-			By("Cleanup the specific resource instance DerivedSecret")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &DerivedSecretReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
+			// If the expected phase is Ready, verify that the derived secret was created with the expected data
+			if derivedSecretPhase == "Ready" {
+				Eventually(func() (map[string][]byte, error) {
+					derivedSecret := &corev1.Secret{}
+					err := k8sClient.Get(ctx, types.NamespacedName{Name: expectedDerivedSecret.Name, Namespace: expectedDerivedSecret.Namespace}, derivedSecret)
+					return derivedSecret.Data, err
+				}).Should(Equal(expectedDerivedSecret.Data))
 			}
 
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
-			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
-		})
+		},
+		Entry("should create the derived secret successfully",
+			&v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "parent-secret",
+					Namespace: "namespace-a",
+				},
+				StringData: map[string]string{
+					"key1": "foo",
+				},
+			},
+			"Ready",
+			"DerivationSuccessful",
+			&v1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "derived-secret",
+					Namespace: "namespace-a",
+				},
+				Data: map[string][]byte{
+					// Derived key from source material foo with salt 'namespace-a/derived-secret' at 32 bytes, base64 encoded should be ZDkwYWMxNTFlOTdhZTkxNGE1ODk2Y2NmNjBlZTA0NDNhZjJhY2U0MmI2YmJjMGY3NDdkYzJjMjY3ODFiYzA4Yw
+					"key1": []byte("GRwhQD1WZM1NvqysEVoTHpq8oH9YV97Fi9VtCURmdHY"),
+				},
+			},
+		),
+	)
+
+	BeforeEach(func() {
+		namespaceA := &v1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "namespace-a",
+			},
+		}
+		Expect(k8sClient.Create(ctx, namespaceA)).To(Succeed())
+	})
+
+	AfterEach(func() {
+		Expect(k8sClient.Delete(ctx, &v1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "namespace-a",
+			},
+		})).To(Succeed())
 	})
 })
